c211108e225da832e5394e2c6223f46acf05a2e0
Mattia Capitanio <mattia.capitanio@gmail.com>
2020-04-02 19:13:12 +0200

    Download from multiple urls

--- transfert-data.sh
+++ transfert-data.sh
@@ -3,15 +3,18 @@ SCRIPTPATH=$(dirname $0)
 
 export $(cat $SCRIPTPATH/.env)
 # TODO: add check for env vars
+KAGGLE_URLS=( $(echo ${KAGGLE_URLS} | tr ";" " ") )
 
 cd $SCRIPTPATH
 source venv/bin/activate
 
-date -u +'%Y-%m-%d %H:%M:%S UTC: Download starts...'
-kaggle datasets download $KAGGLE_URL -p $SCRIPTPATH/$DATA_DIR --unzip --force
-date -u +'%Y-%m-%d %H:%M:%S UTC: Download completed!'
+for url in "${KAGGLE_URLS[@]}"; do
+    date -u +'%Y-%m-%d %H:%M:%S UTC: Download dataset from Kaggle ('${url}')'
+    kaggle datasets download $url -p $SCRIPTPATH/$DATA_DIR --unzip --force
+    date -u +'%Y-%m-%d %H:%M:%S UTC: Download completed!'
+done
 
-date -u +'%Y-%m-%d %H:%M:%S UTC: Upload start...'
+date -u +'%Y-%m-%d %H:%M:%S UTC: Upload datasets on Drive'
 python3 upload.py -d DATA_DIR -g GDRIVE_FOLDER_ID
 date -u +'%Y-%m-%d %H:%M:%S UTC: Upload completed!'
 
